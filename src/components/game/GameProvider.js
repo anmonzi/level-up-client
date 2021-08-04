0295c6462de715e402ec94c76df7fb1f7eccf530
Ashton Monzi <anmonzi@gmail.com>
2021-08-04 11:56:35 -0500

    Ch.7 Done - fetch call retrieves all games from server and displays

---
+++ src/components/game/GameProvider.js
@@ -0,0 +1,28 @@
+import React, { useState, createContext } from 'react'
+
+export const GameContext = createContext()
+
+export const GameProvider = (props) => {
+    const [games, setGames] = useState([])
+
+    const getGames = () => {
+        return fetch("http://localhost:8000/games", {
+            headers:{
+                "Authorization": `Token ${localStorage.getItem("lu_token")}`
+            }
+        })
+        .then(res => res.json())
+        .then(setGames)
+    }
+
+
+    return (
+        <GameContext.Provider value={
+            {
+                games, getGames
+            }
+        }>
+            {props.children}
+        </GameContext.Provider>
+    )
+}
